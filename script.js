35199e13fb29545b145547030faf79e76460014e
Abdul Wahab Tahir <awahabt11@gmail.com>
2024-03-11 23:42:09 +0500

    able for display value

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let value;
+let display = document.getElementById('display_screen');
+let currentInput = '';
 
-function clear(){
-    value = document.getElementById()
+function character(character){
+    currentInput += character;
+    display.value = currentInput;
 }
