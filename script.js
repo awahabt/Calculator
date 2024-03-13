8169bfdb01d1ca41a8bb0005916dead78153e77c
Abdul Wahab Tahir <awahabt11@gmail.com>
2024-03-13 17:34:27 +0500

    Add Functionality

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let display = document.getElementById('display_screen');
+let display = "";
 
 let buttons = document.querySelectorAll('button');
 
-let BtnArray = Array.from(buttons);
-
-
-console.log(BtnArray);
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            display = eval(display);
+            document.querySelector('input').value = display;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            display = "";
+            document.querySelector('input').value = display;
+        }
+        else{
+            console.log(e.target)
+            display= display + e.target.innerHTML;
+            document.querySelector('input').value = display;
+        }
+    })
+})
